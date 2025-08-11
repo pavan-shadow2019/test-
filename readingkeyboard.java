25f1a3db5db819ba8c18323c85955c52ca69dbe4
pavan-shadow2019 <pavanprajapati10148@gmail.com>
2025-08-11 14:14:49 +0530

    reading from keyboard

---
+++ readingkeyboard.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*; /*util is the package to reading keyboard inputs */
+
+public class readingkeyboard{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a, b, c;
+        
+        System.out.print("Enter the First Number: ");
+        a= s.nextInt();
+        
+        System.out.print("Enter the Second Number: ");
+        b = s.nextInt();
+        /* Multipication*/
+        c = a *b;
+        System.out.println("Mulipication Total is " + c);
+
+        s.close();
+    }
+}
