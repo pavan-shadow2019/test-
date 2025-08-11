d7f3db66569358e61d4fe790d65c92091e9c52aa
pavan-shadow2019 <pavanprajapati10148@gmail.com>
2025-08-11 10:33:15 +0530

    added package to make it a habit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+import java.lang.*; /*import lang package, it automatically get imported as it is mandatory package */
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello Bhaiyo");
     }
-    
 }
