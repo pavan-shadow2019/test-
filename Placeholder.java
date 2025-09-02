3c1fe6cd3eb73187d7a3bfac31dc75df7a2a53f3
pavan-shadow2019 <pavanprajapati10148@gmail.com>
2025-09-02 23:21:56 +0530

    formating placeholder printf learned

---
+++ Placeholder.java
@@ -0,0 +1,8 @@
+public class Placeholder{
+    public static void main(String[] args){
+        int age = 22;
+        String name = "Alex";
+        System.out.printf("My name is %s and I am %d years old.", name, age);
+
+    }
+}
