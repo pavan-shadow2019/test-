4bc65681e4bcf2b1ceae0856fce0364aca37ff4b
pavan-shadow2019 <pavanprajapati10148@gmail.com>
2025-09-02 23:24:05 +0530

    added more examples in placeholder formatting printf

--- Placeholder.java
+++ Placeholder.java
@@ -2,7 +2,13 @@ public class Placeholder{
     public static void main(String[] args){
         int age = 22;
         String name = "Alex";
-        System.out.printf("My name is %s and I am %d years old.", name, age);
+        System.out.printf("My name is %s and I am %d years old.\n", name, age);
+
+        System.out.printf("Value of pi: %.2f", 3.14159);
+
+        System.out.printf("\n Hex value: %x", 255);
+
+
 
     }
 }
