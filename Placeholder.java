34198bec5a64f9fe34e5abab6c3e17772bc70897
pavan-shadow2019 <pavanprajapati10148@gmail.com>
2025-09-04 21:58:34 +0530

    added char

--- Placeholder.java
+++ Placeholder.java
@@ -2,7 +2,9 @@ public class Placeholder{
     public static void main(String[] args){
         int age = 22;
         String name = "Alex";
-        System.out.printf("My name is %s and I am %d years old.\n", name, age);
+        char surname = 'P';
+
+        System.out.printf("My name is %s %c and I am %d years old.\n", name, surname, age);
 
         System.out.printf("Value of pi: %.2f", 3.14159);
 
