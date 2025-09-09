093d4324ffaf756d331451d0bb3909d2a818a556
pavan-shadow2019 <pavanprajapati10148@gmail.com>
2025-09-09 21:51:25 +0530

    add other string methods

--- Stringer.java
+++ Stringer.java
@@ -8,15 +8,28 @@ public class Stringer {
     int index = name.indexOf("P"); //index of given character
     int lastIndex = name.lastIndexOf("e"); //index of character placed in the string at the end
     
-    name = name.toUpperCase();
+    //name = name.toUpperCase(); to make all the characters in the string in uppercase
+    name = name.toLowerCase();
+    //name= name.trim(); to remove spaces before and after the words
+    //name = name.replace("o", "a");
+
+
 
     System.out.println(length); 
     System.out.println(letter);
     System.out.println(index);
     System.out.println(lastIndex);
     System.out.println(name);
+    //System.out.println(name.isEmpty()); to check if the variable is empty or not
 
+    if(name.isEmpty()){
+        System.out.println("Your name is Empty");
+    }else{
+        System.out.println("Your name is " + name);
+    }
 
     }
+
+    //name.equals
 
 }
